191f56095fdf05abb9ec060cc29911af10ad0eee
MyroslavPro <myropro@gmail.com>
2021-04-28 18:01:41 +0300

    Added the fixed solution

--- main.py
+++ main.py
@@ -2,27 +2,21 @@ import re
 
 
 def main():
-    time_in_timeframes = False
     counter_of_problems = 0
-    file = open("/home/myr/Python/lab5/access_log_Jul95")
+    file = open("/home/myr/Python/lab5/access_log_Jul95", encoding = "ISO-8859-1")
 
     for line in file:
-        if re.search(r"01/Jul/1995:00:21:\d\d", line) is not None:
-            time_in_timeframes = False
-            break
-        if re.search(r"01/Jul/1995:06:52:\d\d", line) is not None:
-            time_in_timeframes = True
+        if re.search(r"01/Jul/1995:0[1-5]:[0-5][0-9]:\d\d", line) or re.search(r"01/Jul/1995:00:2[1-9]:\d\d",line) or re.search(r"01/Jul/1995:00:[3-5][0-9]:\d\d",line) or re.search(r"01/Jul/1995:06:[0-4][0-9]:\d\d",line) or re.search(r"01/Jul/1995:06:5[0-2]:\d\d",line) is not None:
         
-        if time_in_timeframes and re.search(r"POST", line) and re.search(r" 40[0-4] ", line) is not None:
-            counter_of_problems = counter_of_problems + 1
-        
-        if time_in_timeframes and re.search(r"PUT", line) and re.search(r" 40[0-4] ", line)  is not None:
-            counter_of_problems = counter_of_problems + 1
-        
-        
-        if time_in_timeframes and re.search(r"DELETE", line) and re.search(r" 40[0-4] ", line) is not None:
-            counter_of_problems = counter_of_problems + 1
+            if re.search(r"POST", line) and re.search(r" 40[0-4] ", line) is not None:
+                counter_of_problems = counter_of_problems + 1
 
+            if re.search(r"PUT", line) and re.search(r" 40[0-4] ", line)  is not None:
+                counter_of_problems = counter_of_problems + 1
+
+            if re.search(r"DELETE", line) and re.search(r" 40[0-4] ", line) is not None:
+                counter_of_problems = counter_of_problems +1
+            
     print(f"Number of appeared problems :",counter_of_problems)
 
 
